63e709a4084ddf49e9a8be312b169184002f88e8
Alexander Aberg <alexander_aberg1985@msn.com>
2024-07-22 18:43:17 +0000

    Started building model

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+class Book(models.Model):
+    date = models.DateTimeField()
+    amount = models.PositiveIntegerField(validators=[MinValueValidator(2.0), MaxValueValidator(20.0)])
