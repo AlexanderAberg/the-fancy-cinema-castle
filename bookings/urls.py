18123f2ff22b76b726ec079e3fa07284cbdae193
Alexander Aberg <alexander_aberg1985@msn.com>
2024-07-26 18:09:27 +0000

    Added edit and delete functions for the form

--- bookings/urls.py
+++ bookings/urls.py
@@ -3,4 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.Bookings.as_view(), name='bookings'),
+    path('<slug:slug>/edit_book/<int:book_id>',
+         views.book_edit, name='book_edit'),
+    path('<slug:slug>/delete_book/<int:book_id>',
+         views.book_delete, name='book_delete'),
 ]
--- bookings/views.py
+++ bookings/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
 from django.contrib import messages
+from django.http import HttpResponseRedirect
 from .models import Book
 from .forms import BookForm
 
@@ -22,9 +23,67 @@ def bookings(request):
 
     return render(
         request,
-        b'ookings/bookingmanager.html',
+        'bookings/bookingmanager.html',
         {
             'bookings': bookings,
             'book_form': book_form
         },
     )
+
+
+def book_edit(request, slug, book_id):
+    """
+    Display an individual booking for edit.
+
+    **Context**
+
+    ``booking``
+        An instance of :model:`booking.Post`.
+    ``book``
+        A single comment related to the post.
+    ``book_form``
+        An instance of :form:`bookings.BookForm`
+    """
+    if request.method == "POST":
+
+        queryset = Book.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        book = get_object_or_404(Book, pk=book_id)
+        book_form = BookForm(data=request.POST, instance=book)
+
+        if book_form.is_valid() and book.author == request.user:
+            book = book_form.save(commit=False)
+            book.post = post
+            book.approved = False
+            book.save()
+            messages.add_message(request, messages.SUCCESS, 'Booking Updated!')
+        else:
+            messages.add_message(request, messages.ERROR,
+                                 'Error updating booking!')
+
+    return HttpResponseRedirect(reverse('bookingmanager', args=[slug]))
+
+
+def book_delete(request, slug, book_id):
+    """
+    Delete an individual booking.
+
+    **Context**
+
+    ``booking``
+        An instance of :model:`book.Post`.
+    ``book``
+        A single book related to the post.
+    """
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    book = get_object_or_404(Comment, pk=comment_id)
+
+    if book.author == request.user:
+        book.delete()
+        messages.add_message(request, messages.SUCCESS, 'Booking deleted!')
+    else:
+        messages.add_message(request, messages.ERROR,
+                             'Error with deleting booking')
+
+    return HttpResponseRedirect(reverse('bookingmanager', args=[slug]))
