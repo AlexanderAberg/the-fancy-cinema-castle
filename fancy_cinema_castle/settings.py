a9bc414f3508a0765934d32c0b1c2972134bc15b
Alexander Aberg <alexander_aberg1985@msn.com>
2024-07-30 18:34:45 +0000

    Added local host to allowed_hosts

--- fancy_cinema_castle/settings.py
+++ fancy_cinema_castle/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-alexanderab-thefancycin-gqrsaz7a32j.ws.codeinstitute-ide.net','.herokuapp.com']
+ALLOWED_HOSTS = ['8000-alexanderab-thefancycin-gqrsaz7a32j.ws.codeinstitute-ide.net','.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
