9dd63d1c6053bf6484ed0f76b0311b79b8d89d2d
Alexander Aberg <alexander_aberg1985@msn.com>
2024-07-31 19:52:03 +0000

    Removed commented out code in admin.py

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
 from .models import Book
-#from .models import Options
 
 # Register your models here.
 @admin.register(Book)
@@ -11,6 +10,3 @@ class BookAdmin(SummernoteModelAdmin):
     search_fields = ['user', 'content']
     list_filter = ('date',)
     summernote_fields = ('content',)
-
-
-#admin.site.register(Options)
