caabc5e96491c8ea98bd84de1295098d61605552
Alexander Aberg <alexander_aberg1985@msn.com>
2024-08-12 16:31:01 +0000

    Made naming be more consistent

--- bookings/urls.py
+++ bookings/urls.py
@@ -3,8 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.bookings, name='bookings'),
-    path('edit_book/<int:book_id>',
-         views.book_edit, name='book_edit'),
-    path('delete_book/<int:book_id>',
-         views.book_delete, name='book_delete'),
+    path('book_edit/<int:book_id>/', views.book_edit, name='book_edit'),
+    path('delete_book/<int:book_id>/', views.book_delete, name='book_delete'),
 ]
