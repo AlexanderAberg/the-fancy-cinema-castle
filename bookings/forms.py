d5d70c5f04bc704809600cbade53c1c68bcd8c7d
Alexander Aberg <alexander_aberg1985@msn.com>
2024-08-12 16:26:56 +0000

    Made calendar for date + time, and removed commented out code

--- bookings/forms.py
+++ bookings/forms.py
@@ -1,14 +1,8 @@
 from django import forms
 from .models import Book
-#from .models import Options
 
 #Creates a form based on the model.
 class BookForm(forms.ModelForm):
-
- #  options = forms.ModelMultipleChoiceField(
-  #     queryset=Options.objects.all(),
-   #    widget=forms.CheckboxSelectMultiple
-    #)
 
     class Meta:
         model = Book
@@ -20,5 +14,3 @@ class BookForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        
- #   self.fields['options'] = forms.MultipleChoiceField()
