e10dd1c66ee9818ddec743f01e599e47d4dd3661
Alexander Aberg <alexander_aberg1985@msn.com>
2024-08-14 14:04:59 +0000

    Test 404 page

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf.urls import handler404
 
 
 urlpatterns = [
     path('', views.home, name='home'),
 ]
+
+
+handler404 = views.custom_404_view
--- home/views.py
+++ home/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from cloudinary.models import CloudinaryField
 
 # Create your views here.
 def home(request):
@@ -13,3 +12,6 @@ def home(request):
         "home/index.html",
         {"home": home},
     )
+
+def custom_404_view(request, exception):
+    return render(request, '404.html', {}, status=404)
