5537335c8acba165da80dfbdfeee874027127da2
Alexander Aberg <alexander_aberg1985@msn.com>
2024-08-14 14:11:21 +0000

    Moved 404 from home to main urls

--- fancy_cinema_castle/urls.py
+++ fancy_cinema_castle/urls.py
@@ -1,21 +1,8 @@
-"""
-URL configuration for fancy_cinema_castle project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+#URL for The Fancy Cinema Castle
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import handler404
+from home.views import custom_404_view
 
 urlpatterns = [
     path('about/', include('about.urls'), name='about-urls'),
@@ -25,3 +12,6 @@ urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
     path('', include('home.urls'), name='home-urls'),
 ]
+
+
+handler404 = custom_404_view
--- home/urls.py
+++ home/urls.py
@@ -1,11 +1,7 @@
 from django.urls import path
 from . import views
-from django.conf.urls import handler404
 
 
 urlpatterns = [
     path('', views.home, name='home'),
 ]
-
-
-handler404 = views.custom_404_view
